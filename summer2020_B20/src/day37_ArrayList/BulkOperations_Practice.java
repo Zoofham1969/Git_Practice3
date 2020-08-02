848051c81625efcef7414a04088e0c37a9c2a893
Zoofham1969 <parwani0602@gmail.com>
2020-08-02 13:10:55 -0500

    day37 practice

---
+++ summer2020_B20/src/day37_ArrayList/BulkOperations_Practice.java
@@ -0,0 +1,27 @@
+package day37_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BulkOperations_Practice {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> students = new ArrayList<>();
+        students.add("Sayeem");
+        students.add("Waqar");
+        students.add("Beslan");
+        students.add("Daud");
+        students.add("Ramazan");
+        students.add("Mehary");
+
+        //verify the names Ulku,Busra ara containedin students list
+
+
+
+
+    }
+
+
+}
