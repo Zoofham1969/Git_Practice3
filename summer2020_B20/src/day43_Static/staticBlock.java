c6d891ee9d70777cc1731816b546a294505156e3
Zoofham1969 <parwani0602@gmail.com>
2020-08-22 23:46:09 -0500

    day43 Static

---
+++ summer2020_B20/src/day43_Static/staticBlock.java
@@ -0,0 +1,21 @@
+package day43_Static;
+
+public class staticBlock {
+   /*
+    static {
+        System.out.println("static block");
+    }
+*/
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+
+
+    static {
+        System.out.println("static block");
+    }
+
+
+
+
+}
